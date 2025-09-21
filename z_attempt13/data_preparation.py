af2f49571b23b7f0228230d9cb8f7531b0d44074
Junhan <junhan.loh@podium.io>
2025-09-21 23:27:43 +0800

    u

--- z_attempt13/data_preparation.py
+++ z_attempt13/data_preparation.py
@@ -103,4 +103,6 @@ df = create_holiday_feature(df)
 df = create_rain_wind_features(df)
 
 print( "Final shape : " + str(df.shape) )
-df.to_csv('resources/13_Building_A.csv', index=False)
+import os
+parent = os.path.dirname(__file__)
+df.to_csv(os.path.join(parent, 'Building_A.csv'), index=False)
